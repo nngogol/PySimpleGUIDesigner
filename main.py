96fd27288e5fed05156d522e1c3c6aa6afec80c3
nngogol <nngogol09@gmail.com>
2019-05-23 08:41:52 +0300

    pr

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from boilerplate_psg import boilerplate_
 from platform import system as gimme_system
-from re import compile as regex, finditer
+from re import compile as make_regex, finditer, MULTILINE
 from shutil import copy as copyfile
 import click
 import json
 import os
-import PySimpleGUI as sg
 import re
-import subprocess
 import sys
+import subprocess
+import PySimpleGUI as sg
 from transpiler import *
 cd = os.path.dirname(os.path.abspath(__file__))
+
 
 def build_boilerplate(layout='[[]]', mouse_clicks=False, keys=False):
 	# create a str with PSG code
@@ -20,7 +21,7 @@ def build_boilerplate(layout='[[]]', mouse_clicks=False, keys=False):
 
 	def do_mice_events(ui, boilerplate):
 		# find regex pattern from all buttons
-		regex_pattern = regex(
+		regex_pattern = make_regex(
 			r'(RButton|ReadButton|B)\([\'\"][\w\ \_\d]*[\'\"],?\s?((key)\s*=\s*[\'\"]([\w\ \_\d]+?)[\'\"])')
 		# find all keys in you ui
 		keys = [i.group(4) for i in finditer(regex_pattern, ui)]
@@ -28,7 +29,7 @@ def build_boilerplate(layout='[[]]', mouse_clicks=False, keys=False):
 
 	def do_keys_events(ui, boilerplate):
 		# find regex pattern from all elements
-		regex_pattern = regex(r'(key)\s*=\s*[\'\"]([\w\d]+?)[\'\"]')
+		regex_pattern = make_regex(r'(key)\s*=\s*[\'\"]([\w\d]+?)[\'\"]')
 		# find all keys in you ui
 		keys = [i.group(2) for i in finditer(regex_pattern, ui)]
 		return boilerplate.replace('# ~TARGET', do_callbacks(keys))
@@ -74,7 +75,8 @@ def just_compile(values):
 	NO_BAD_WIDGETS = '1' if NO_BAD_WIDGETS else '0'
 	command = '{0} "{1}" {2} {3}'.format(
 		python, psg_ui_maker__py, OBJ_NAME, NO_BAD_WIDGETS)
-	subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+	subprocess.run(command, shell=True, stdout=subprocess.DEVNULL,
+				   stderr=subprocess.DEVNULL)
 	# 3
 	os.remove(ui_file)
 	# 4
@@ -94,7 +96,7 @@ def run_gui():
 		'''
 		clear ui to easyily for coping functionality (ctrl+c)
 		'''
-		
+
 		'''
 		# case1
 		sg.Frame('', key='gridLayout', layout = [
@@ -102,7 +104,8 @@ def run_gui():
 		])
 		'''
 		first_line = ui.split('\n')[0]
-		regex_matched = re.compile(r"^sg.Frame\('',\s?key='.*',\slayout\s=\s\[").match(first_line)
+		regex_matched = make_regex(
+			r"^sg.Frame\('',\s?key='.*',\slayout\s=\s\[").match(first_line)
 		if regex_matched and ui[-2:] == '])':
 			new_ui = '[\n' + '\n'.join(ui.split('\n')[1:]).strip()
 			return new_ui[:-1]
@@ -128,37 +131,42 @@ def run_gui():
 	#  |___/
 
 	ralign = {'size': (16, 3), "justification": 'r'}
-	input_frame = [[sg.T('\nxml file', **ralign), 					sg.In(key='xmlfile', change_submits=True),	sg.FileBrowse(target='xmlfile'), 	sg.T('possible\nobject names', justification='r'), sg.InputCombo(values=[''], key='objs', size=(40,1), change_submits=True)],
-				   [sg.T('\nTarget object name', **ralign), 		sg.In(key='objname', change_submits=True),	sg.B('compile', key='compile_btn', disabled=True), sg.B('compile++', key='compilepp_btn', disabled=True),
-					sg.Radio('all keys', 1, True, key='r2_keys'), 	sg.Radio('mouse clicks', 1, key='r2_mouse_clicks')]]
+	input_frame = [[sg.T('\nxml file', **ralign),                   sg.In(key='xmlfile', change_submits=True),  sg.FileBrowse(target='xmlfile'),    sg.T('possible\nobject names', justification='r'), sg.InputCombo(values=[''], key='objs', size=(40, 1), change_submits=True)],
+				   [sg.T('\nTarget object name', **ralign),         sg.In(key='objname', change_submits=True),  sg.B('compile', key='compile_btn', disabled=True), sg.B('compile++', key='compilepp_btn', disabled=True),
+					sg.Radio('all keys', 1, True, key='r2_keys'),   sg.Radio('mouse clicks', 1, key='r2_mouse_clicks')]]
 	layout = [
 		[sg.Frame('Input data', input_frame)],
 		[sg.B('Clear'),
-			sg.CB('forget about bad widgets', True, key='no_bad_widgets'),
-			sg.CB('empty top widget', True, key='empty_top_widget')],
+		 sg.CB('forget about bad widgets', True, key='no_bad_widgets'),
+		 sg.CB('empty top widget', True, key='empty_top_widget')],
 		[sg.Multiline(key='psg_ui_output', size=(120, 14))]
 	]
 	window = sg.Window('Transpiler', layout,
 					   auto_size_buttons=False,
 					   default_button_element_size=(10, 1))
 
-
 	while True:             # Event Loop
 		event, values = window.Read()
-		
-		if event in (None, 'Exit'): break
+
+		if event in (None, 'Exit'):
+			break
 		elif event == 'xmlfile':
 			myxml_file = values['xmlfile']
 			if os.path.exists(myxml_file):
 
 				# get xml
-				with open(myxml_file, 'r', encoding='utf-8') as ff: xml_code = ff.read()
+				with open(myxml_file, 'r', encoding='utf-8') as ff:
+					xml_code = ff.read()
 
 				# filter object names
-				widgets_regexpattern = re.compile(r"^[ \s]{1,}<(widget)\s?.*?\s?name=\"(.+)\"\/?>", re.MULTILINE)
-				layouts_regexpattern = re.compile(r"^[ \s]{1,}<(layout)\s?.*?\s?name=\"(.+)\"\/?>", re.MULTILINE)
-				widgets = [i.group(2) for i in re.finditer(widgets_regexpattern, xml_code)]
-				layouts = [i.group(2) for i in re.finditer(layouts_regexpattern, xml_code)]
+				widgets_regexpattern = make_regex(
+					r"^[ \s]{1,}<(widget)\s?.*?\s?name=\"(.+)\"\/?>", MULTILINE)
+				layouts_regexpattern = make_regex(
+					r"^[ \s]{1,}<(layout)\s?.*?\s?name=\"(.+)\"\/?>", MULTILINE)
+				widgets = [i.group(2) for i in finditer(
+					widgets_regexpattern, xml_code)]
+				layouts = [i.group(2) for i in finditer(
+					layouts_regexpattern, xml_code)]
 
 				combo_items = ['# LAYOUTS widgets #', *layouts, '# WIDGETS widgets #', *widgets]
 
@@ -171,20 +179,23 @@ def run_gui():
 					window.Element('objname').Update(el)
 					update_clear_btn(window, values, real_value=el)
 
-		elif event == 'objs': 
+		elif event == 'objs':
 			# add only REAL object names -> those, who not contain ' '
 			if ' ' not in values['objs']:
 				window.Element('objname').Update(values['objs'])
 			update_clear_btn(window, values, real_value=values['objs'])
-		elif event == 'objname': 	update_clear_btn(window, values)
-		elif event == 'Clear': 		window.Element('psg_ui_output').Update('')
-		elif event == 'compile_btn': 
+		elif event == 'objname':
+			update_clear_btn(window, values)
+		elif event == 'Clear':
+			window.Element('psg_ui_output').Update('')
+		elif event == 'compile_btn':
 			ui = just_compile(values)
 
-			if values['empty_top_widget']: ui = clear_empty_top_widget(ui)
+			if values['empty_top_widget']:
+				ui = clear_empty_top_widget(ui)
 
 			window.Element('psg_ui_output').Update(ui)
-		elif event == 'compilepp_btn': 
+		elif event == 'compilepp_btn':
 			ui = just_compile(values)
 
 			# case for 'speed up'
@@ -193,31 +204,33 @@ def run_gui():
 
 			ui = just_compile(values)
 
-			if values['empty_top_widget']: ui = clear_empty_top_widget(ui)
+			if values['empty_top_widget']:
+				ui = clear_empty_top_widget(ui)
 
-			psg_ui = build_boilerplate( layout=ui,
-										mouse_clicks=values['r2_mouse_clicks'],
-										keys=values['r2_keys'])
+			psg_ui = build_boilerplate(layout=ui,
+									   mouse_clicks=values['r2_mouse_clicks'],
+									   keys=values['r2_keys'])
 			window.Element('psg_ui_output').Update(psg_ui)
-		
+
 		elif event == 'Try in PySimpleGUI':
 			pass
-		# 	try:
-		# 		ui = values['psg_ui_output'].strip()
+		#   try:
+		#       ui = values['psg_ui_output'].strip()
 
-		# 		if ui[:18] == "sg.Frame('', key='" and ui[-2:] == "])":
-		# 			ui = ui[ui.index('['):-1]
-		# 		elif ui[0] == "[" and ui[-1] == "]":
-		# 			pass
+		#       if ui[:18] == "sg.Frame('', key='" and ui[-2:] == "])":
+		#           ui = ui[ui.index('['):-1]
+		#       elif ui[0] == "[" and ui[-1] == "]":
+		#           pass
 
-		# 		window2 = sg.Window('test', myui)
-		# 		window2.Read()
-		# 		window2.Close()
+		#       window2 = sg.Window('test', myui)
+		#       window2.Read()
+		#       window2.Close()
 
-		# 	except Exception as e:
-		# 		mbox(str(e))
+		#   except Exception as e:
+		#       mbox(str(e))
 
 	window.Close()
+
 
 @click.command()
 @click.option('-x', '--run', default=True, is_flag=True, help='just run gui example')
@@ -229,12 +242,13 @@ def run_gui():
 @click.option('-pp_keys',  default=False, is_flag=True, help='compile++ option - do the keys events')
 def cli(run, xmlfile, objname, nobadwidgets, outputfile, pp_mouse, pp_keys):
 
-	if run and not (xmlfile and objname): run_gui()
+	if run and not (xmlfile and objname):
+		run_gui()
 	elif xmlfile and objname:
 
 		try:
 			psg_ui = just_compile({'objname': objname,
-				'xmlfile': xmlfile, 'no_bad_widgets': nobadwidgets })
+								   'xmlfile': xmlfile, 'no_bad_widgets': nobadwidgets})
 			# compile++
 			if pp_mouse:
 				psg_ui = build_boilerplate(layout=psg_ui, mouse_clicks=True)
@@ -250,6 +264,7 @@ def cli(run, xmlfile, objname, nobadwidgets, outputfile, pp_mouse, pp_keys):
 			# click.echo(click.style("\n~~~done", bg='black', fg='green'))
 		except Exception as e:
 			click.echo(click.style(str(e), bg='black', fg='red'))
+
 
 if __name__ == '__main__':
 	cli()
